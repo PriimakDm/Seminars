7554a240a1942d59b90be8f6e2b0f1ab09953a5a
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-15 12:42:51 +0700

    Task_1

---
+++ Seminar_1/Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("input namber1: ");
+int namber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input namber2: ");
+int namber2 = Convert.ToInt32(Console.ReadLine());
+int max = namber1;
+if (namber2 > max) max = namber2;
+Console.Write ("max= ");
+Console.WriteLine (max);
