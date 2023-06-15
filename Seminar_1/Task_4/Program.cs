4c8cd89f9db1eb2b7846cf0ef4c1ca280073d612
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-15 15:06:43 +0700

    Task_4

--- Seminar_1/Task_4/Program.cs
+++ Seminar_1/Task_4/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Вывести четные числа от одного до N
 // int count=2
-Console.WriteLine("input namber: ");
+Console.WriteLine("input namber >= 2: ");
 int namber = Convert.ToInt32(Console.ReadLine());
 int count=2;
 while (count <= namber)
