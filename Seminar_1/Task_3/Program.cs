d1f51758c0a45c8f6277db2a52277aa6b5b46332
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-15 14:54:55 +0700

    Task_4

--- Seminar_1/Task_3/Program.cs
+++ Seminar_1/Task_3/Program.cs
@@ -1,11 +1,11 @@
-﻿// Задача на четность чиса
+﻿// Задача на четность числа
 Console.WriteLine("input namber: ");
 int namber = Convert.ToInt32(Console.ReadLine());
 if ((namber % 2) == 0)
 {
-Console.WriteLine("an even number");
+    Console.WriteLine("an even number");
 }
 else
 {
-Console.WriteLine("odd number");
+    Console.WriteLine("odd number");
 }
---
+++ Seminar_1/Task_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывести четные числа от одного до N
+// int count=2
+Console.WriteLine("input namber: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+int count=2;
+while (count <= namber)
+{
+    Console.Write(count + ", ");
+    count = count+2;
+}
