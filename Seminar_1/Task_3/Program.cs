fe3eb61142a5ac3da53c673dc5ebca0ed127e3d9
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-15 14:03:21 +0700

    Task_3

---
+++ Seminar_1/Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача на четность чиса
+Console.WriteLine("input namber: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+if ((namber % 2) == 0)
+{
+Console.WriteLine("an even number");
+}
+else
+{
+Console.WriteLine("odd number");
+}
