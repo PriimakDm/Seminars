640a771be411ffeb66196d7a334c9414fa810ac5
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-15 13:09:08 +0700

    Task_2

--- Seminar_1/Task_2/Program.cs
+++ Seminar_1/Task_2/Program.cs
@@ -1,9 +1,12 @@
-﻿// Задача на вывод максимального числа из двух чисел
+﻿// Задача на вывод максимального числа из трех чисел
 Console.WriteLine("input namber1: ");
 int namber1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input namber2: ");
 int namber2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input namber3: ");
+int namber3 = Convert.ToInt32(Console.ReadLine());
 int max = namber1;
 if (namber2 > max) max = namber2;
+if (namber3 > max) max = namber3;
 Console.Write ("max= ");
 Console.WriteLine (max);
